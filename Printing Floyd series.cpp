5e653236141d0905382e928a8ff9bc3e0bc1a1e1
vaishnavipangare <vaishnavi.pangare.btech2024@sitpune.edu.in>
2025-07-30 18:22:01 -1200

    Create Printing Floyd series.cpp

---
+++ Printing Floyd series.cpp
@@ -0,0 +1,23 @@
+//EXPERIMENT-6
+//Vaishnavi Pangare
+//25070123501
+//ENTC A3
+//TITLE: Print Floyd series.
+
+#include <iostream>
+using namespace std;
+int i,j;
+int n=4;
+int k=1;
+
+int main(){
+}
+
+
+/*Output:-
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+
+*/
